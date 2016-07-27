3581c2b435216edac54134ccd3c70428dc47d60d
Brandon Smith <brandon93s@gmail.com>
2016-07-26 23:43:03 -0400

    Implement basic table conversion

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 'use strict';
 
-module.exports = function (str, opts) {
-  if (typeof str !== 'string') {
+const cheerio = require('cheerio');
+
+module.exports = function (html, options) {
+  if (typeof html !== 'string') {
     throw new TypeError('Expected a string');
   }
 
-  opts = opts || {};
+  options = options || {};
 
-  return str + ' & ' + (opts.postfix || 'rainbows');
+  const $ = cheerio.load(html);
+
+  let results = [];
+  $('table').each((i, element) => results.push(processTable($, element, options)));
+
+  if (options.unwrapTable)
+    return results[0];
+
+  return results;
 };
+
+function processTable($, table, options) {
+  const headers = buildHeaders($, table, options);
+
+  let tableJson = [];
+  $(table).find('tr').each((i, element) => tableJson.push(processRow($, element, options, headers)));
+
+  tableJson = pruneEmptyRows(tableJson);
+
+  return tableJson;
+}
+
+function processRow($, row, options, headers) {
+  let rowJson = {};
+
+  $(row).find('td').each((i, cell) => {
+    rowJson[headers[i] || (i + 1)] = $(cell).text().trim();
+  });
+
+  return rowJson;
+}
+
+function buildHeaders($, table, options) {
+  let headers = [];
+
+  $(table).find('tr').each((i, row) => {
+    $(row).find('th').each((j, cell) => {
+      headers[j] = $(cell).text().trim();
+    });
+  });
+
+  return headers;
+}
+
+function pruneEmptyRows(table) {
+  return table.filter(t => Object.keys(t).length);
+}
--- test.js
+++ test.js
@@ -1,6 +1,16 @@
-import test from 'ava';
-import fn from './';
+'use strict';
 
-test('title', t => {
-  t.is(fn('unicorns'), 'unicorns & rainbows');
-});
+import test from 'ava';
+import sut from './';
+
+/* Input Validation */
+function typeThrows(t, input) {
+  t.throws(() => sut(input));
+}
+typeThrows.title = (providedTitle, input) => `${typeof input} throws`;
+
+test(typeThrows, {});
+test(typeThrows, []);
+test(typeThrows, 1);
+test(typeThrows, NaN);
+test(typeThrows, true);
