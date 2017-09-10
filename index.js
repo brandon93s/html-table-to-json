f27dae9630be9ef35f230a00bc9d4fe10dabec51
Brandon Smith <brandon93s@gmail.com>
2017-09-09 20:47:36 -0400

    Update dependencies, code styling

--- index.js
+++ index.js
@@ -1,73 +1,71 @@
-'use strict';
+'use strict'
 
-const cheerio = require('cheerio');
+const cheerio = require('cheerio')
 
 class HtmlTableToJson {
-  constructor(html, opts) {
-    if (typeof html !== 'string')
-      throw new TypeError('html input must be a string');
+  constructor (html, opts) {
+    if (typeof html !== 'string') { throw new TypeError('html input must be a string') }
 
-    this.html = html;
-    this.opts = opts;
+    this.html = html
+    this.opts = opts
 
-    this._$ = cheerio.load(this.html);
-    this._results = [];
-    this._headers = [];
-    this._count = null;
+    this._$ = cheerio.load(this.html)
+    this._results = []
+    this._headers = []
+    this._count = null
 
-    this._process();
+    this._process()
   }
 
-  static factory(html, opts) {
-    return new HtmlTableToJson(html, opts);
+  static factory (html, opts) {
+    return new HtmlTableToJson(html, opts)
   }
 
-  get count() {
-    return Number.isInteger(this._count) ? this._count : (this._count = this._$('table').get().length);
+  get count () {
+    return Number.isInteger(this._count) ? this._count : (this._count = this._$('table').get().length)
   }
 
-  get results() {
-    return this._results;
+  get results () {
+    return this._results
   }
 
-  _process() {
-    if (this._results.length)
-      return this._results;
+  _process () {
+    if (this._results.length) { return this._results }
 
-    this._$('table').each((i, element) => this._processTable(i, element));
+    this._$('table').each((i, element) => this._processTable(i, element))
 
-    return this._results;
+    return this._results
   }
 
-  _processTable(tableIndex, table) {
-    this._results[tableIndex] = [];
-    this._buildHeaders(tableIndex, table);
+  _processTable (tableIndex, table) {
+    this._results[tableIndex] = []
+    this._buildHeaders(tableIndex, table)
 
-    this._$(table).find('tr').each((i, element) => this._processRow(tableIndex, i, element));
-    this._pruneEmptyRows(tableIndex);
+    this._$(table).find('tr').each((i, element) => this._processRow(tableIndex, i, element))
+    this._pruneEmptyRows(tableIndex)
   }
 
-  _processRow(tableIndex, index, row) {
-    this._results[tableIndex][index] = {};
+  _processRow (tableIndex, index, row) {
+    this._results[tableIndex][index] = {}
 
     this._$(row).find('td').each((i, cell) => {
-      this._results[tableIndex][index][this._headers[tableIndex][i] || (i + 1)] = this._$(cell).text().trim();
-    });
+      this._results[tableIndex][index][this._headers[tableIndex][i] || (i + 1)] = this._$(cell).text().trim()
+    })
   }
 
-  _buildHeaders(index, table) {
-    this._headers[index] = [];
+  _buildHeaders (index, table) {
+    this._headers[index] = []
 
     this._$(table).find('tr').each((i, row) => {
       this._$(row).find('th').each((j, cell) => {
-        this._headers[index][j] = this._$(cell).text().trim();
-      });
-    });
+        this._headers[index][j] = this._$(cell).text().trim()
+      })
+    })
   }
 
-  _pruneEmptyRows(tableIndex) {
-    this._results[tableIndex] = this._results[tableIndex].filter(t => Object.keys(t).length);
+  _pruneEmptyRows (tableIndex) {
+    this._results[tableIndex] = this._results[tableIndex].filter(t => Object.keys(t).length)
   }
 }
 
-module.exports = HtmlTableToJson;
+module.exports = HtmlTableToJson
